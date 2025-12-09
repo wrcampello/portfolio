a72da15e9a12273b2a1de7aabce8164053772660
Wilson Campello <wilson.campello@hospitalsaocamilosp.org.br>
2025-12-09 20:46:14 -0300

    Adiciona sitemap.xml e robots.txt para melhorar indexação no Google

---
+++ src/app/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://wilcampellodev.vercel.app';
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
+
